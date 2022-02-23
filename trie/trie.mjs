b082238b32e05ef12cae70349c6d26ca7642d926
gamja <googolhkl@gmail.com>
2022-02-23 17:38:07 +0900

    트라이 검색 빈도로 검색되도록 업데이트

--- trie/trie.mjs
+++ trie/trie.mjs
@@ -1,3 +1,5 @@
+import { Heap } from "./../heap/heap.mjs"
+
 class Node{
     constructor(){
         this.children = {}; // 자식 노드들
@@ -22,10 +24,10 @@ class Trie{
             }
         }
 
-        currentNode.children["*"] = null; // 모든 문자를 추가했다면 종료문자 *를 삽입
+        currentNode.children["*"] = 0; // 모든 문자를 추가했다면 종료문자 *를 삽입
     }
 
-    search(word){
+    search(word, isCounting = false){
         let currentNode = this.root;
 
         for(let char of word){
@@ -34,6 +36,10 @@ class Trie{
             }else{
                 return null
             }
+        }
+
+        if(isCounting == true){
+            currentNode.children["*"]++;
         }
 
         return currentNode;
@@ -48,7 +54,7 @@ class Trie{
         for(let key in currentNode.children){
             let childNode = currentNode.children[key];
             if(key == "*"){
-                words.push(word);
+                words.push(new WordData(word, childNode));
             }else{
                 this.getAllWords(childNode, word + key, words);
             }
@@ -66,6 +72,39 @@ class Trie{
 
         return this.getAllWords(currentNode, word);
     }
+
+    autoCompleteByCount(word){
+        let words = trie.autoComplete(word); // 자동완성 단어들
+
+        // 힙 정렬(내림차순)
+        let heap = new Heap();
+        heap.isBigPriority = function(first, second){
+            return (first.priority > second.priority);
+        }
+        for(let word of words){
+            heap.insert(word);
+        }
+
+        let sortedBySearchCount = [];
+        do{ // 힙에서 꺼내면서 배열에 삽입
+            let removed = heap.remove();
+            if(removed == null){
+                break;
+            }else{
+                sortedBySearchCount.push(removed);
+            }
+        }while(true)
+
+        return sortedBySearchCount;
+    }
+}
+
+class WordData{
+    constructor(word, count){
+        this.word = word;
+        this.count = count;
+        this.priority = count;
+    }
 }
 
 
@@ -78,4 +117,12 @@ trie.insert("감자볶음");
 trie.insert("김치찌개");
 trie.insert("두부");
 
-console.log(trie.autoComplete("감자"));
+trie.search("감자", true); // 유저들이 검색을함(counting됨)
+trie.search("감자", true);
+trie.search("감자", true);
+trie.search("감자부침개", true);
+trie.search("감자부침개", true);
+
+
+
+console.log(trie.autoCompleteByCount("감자"));
